52848965c6580f06d6e23829eb74e3a80cde964c
UmutKzl <umutkzl@protonmail.com>
2026-04-26 18:37:06 +0300

    feat: move fonts to font.nix and add some fonts

--- modules/font.nix
+++ modules/font.nix
@@ -1,4 +1,6 @@
 {pkgs, ...}: {
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [nerd-fonts.jetbrains-mono noto-fonts noto-fonts-cjk-sans liberation_ttf];
   gtk = {
     font = {
       name = "JetBrainsMono Nerd Font";
