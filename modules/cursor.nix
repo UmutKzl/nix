1965cd9af0ccdb7bbc60df798c58006b4abc87b4
UmutKzl <umutkzl@protonmail.com>
2026-04-28 19:18:06 +0300

    chore: remove x11.enable in cursor.nix

--- modules/cursor.nix
+++ modules/cursor.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   home.pointerCursor = {
     gtk.enable = true;
-    x11.enable = true;
     package = pkgs.catppuccin-cursors.latteDark;
     name = "Catppuccin Latte Dark";
     size = 16;
