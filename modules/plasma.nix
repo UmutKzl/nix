e37239968a06ad85b4e92d633da99848750a0d9a
UmutKzl <umutkzl@protonmail.com>
2026-04-30 21:20:51 +0300

    feat: update plasma config

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -1,21 +1,54 @@
 {pkgs, ...}: {
+  home.packages = [pkgs.catppuccin-kde]; # install catppuccin
+
   programs.plasma = {
     enable = true;
+
     workspace = {
       clickItemTo = "open";
-      lookAndFeel = "org.kde.breezedark.desktop";
-      # wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Mountain/contents/images_dark/5120x2880.jpg";
+      cursor.theme = "Catppuccin-Frappe-Blue-Cursors";
+      windowDecorations = {
+        library = "org.kde.breeze";
+        theme = "Breeze";
+      };
       wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Mountain/contents/images/mountain.jpg";
     };
+
+    kwin.virtualDesktops = {
+      number = 10;
+    };
+
     shortcuts = {
       "kwin" = {
         "Overview" = "none";
+
         "Window Close" = "Meta+Q";
         "Window Maximize" = "Meta+W";
+
         "Window Quick Tile Left" = "Meta+H";
         "Window Quick Tile Bottom" = "Meta+J";
         "Window Quick Tile Top" = "Meta+K";
         "Window Quick Tile Right" = "Meta+L";
+
+        "Switch to Desktop 1" = "Meta+1";
+        "Switch to Desktop 2" = "Meta+2";
+        "Switch to Desktop 3" = "Meta+3";
+        "Switch to Desktop 4" = "Meta+4";
+        "Switch to Desktop 5" = "Meta+5";
+        "Switch to Desktop 6" = "Meta+6";
+        "Switch to Desktop 7" = "Meta+7";
+        "Switch to Desktop 8" = "Meta+8";
+        "Switch to Desktop 9" = "Meta+9";
+
+        "Window to Desktop 1" = "Meta+!";
+        "Window to Desktop 2" = "Meta+@";
+        "Window to Desktop 3" = "Meta+#";
+        "Window to Desktop 4" = "Meta+$";
+        "Window to Desktop 5" = "Meta+ %";
+        "Window to Desktop 6" = "Meta+^";
+        "Window to Desktop 7" = "Meta+&";
+        "Window to Desktop 8" = "Meta+*";
+        "Window to Desktop 9" = "Meta+(";
       };
       "ksmserver" = {
         "Lock Session" = "Ctrl+Meta+Q";
@@ -30,6 +63,7 @@
         "RectangularRegionScreenShot" = "Meta+Shift+S";
       };
     };
+
     input = {
       touchpads = [
         {
@@ -43,12 +77,32 @@
         }
       ];
     };
+
     panels = [
       {
         location = "top";
         floating = false;
         alignment = "center";
         lengthMode = "fill";
+        widgets = [
+          "org.kde.plasma.kickoff"
+          {
+            name = "org.kde.plasma.icontasks";
+            config = {
+              General = {
+                launchers = [
+                  "applications:com.mitchellh.ghostty.desktop"
+                  "applications:org.kde.dolphin.desktop"
+                  "applications:firefox.desktop"
+                ];
+              };
+            };
+          }
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.pager"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+        ];
       }
     ];
   };
