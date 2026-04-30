6ef61c83684afdac3f4c85e8da982bb4d1f688cf
UmutKzl <umutkzl@protonmail.com>
2026-04-30 20:28:58 +0300

    chore: disable hyprland to not conflict with kde plasma

--- modules/default.nix
+++ modules/default.nix
@@ -1,17 +1,18 @@
 {pkgs, ...}: {
   imports = [
-    ./cursor.nix
     ./env.nix
     ./font.nix
     ./fuzzel.nix
-    ./mako.nix
     ./ghostty.nix
-    ./icons.nix
-    ./hyprland.nix
-    ./theme.nix
     ./nvim.nix
-    ./waybar.nix
+    ./plasma.nix
     ./zsh.nix
     ./git.nix
+    # ./mako.nix
+    # ./cursor.nix
+    # ./icons.nix
+    # ./hyprland.nix
+    # ./waybar.nix
+    # ./theme.nix
   ];
 }
