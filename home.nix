0ee6557448684cdf002edaff3402f0a7299c98d5
UmutKzl <umutkzl@protonmail.com>
2026-04-30 20:27:52 +0300

    chore: move home. things to home = {};

--- home.nix
+++ home.nix
@@ -9,10 +9,12 @@
     ./packages.nix
   ];
 
-  home.username = lib.mkDefault "umutkzl";
-  home.homeDirectory = lib.mkDefault "/home/umutkzl";
-  home.stateVersion = "25.11";
-  home.enableNixpkgsReleaseCheck = false;
+  home = {
+    username = lib.mkDefault "umutkzl";
+    homeDirectory = lib.mkDefault "/home/umutkzl";
+    stateVersion = "25.11";
+    enableNixpkgsReleaseCheck = false;
+  };
 
   programs.home-manager.enable = true;
 }
