1bd5250d5f94a286801471a43c2f5048b80226dd
UmutKzl <umutkzl@protonmail.com>
2026-05-02 19:20:23 +0300

    stuff: add comments

--- home.nix
+++ home.nix
@@ -4,11 +4,13 @@
   lib,
   ...
 }: {
+  # Import Things
   imports = [
     ./modules
     ./packages.nix
   ];
 
+  # Some things
   home = {
     username = lib.mkDefault "umutkzl";
     homeDirectory = lib.mkDefault "/home/umutkzl";
@@ -16,7 +18,9 @@
     enableNixpkgsReleaseCheck = false;
   };
 
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Let Home-Manager manage itself
   programs.home-manager.enable = true;
 }
