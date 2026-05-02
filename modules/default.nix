692283d382d0597feff15c79d00ec48a2630d4b3
UmutKzl <umutkzl@protonmail.com>
2026-05-02 19:16:09 +0300

    refactor: make default.nix readable

--- modules/default.nix
+++ modules/default.nix
@@ -1,13 +1,19 @@
 {pkgs, ...}: {
   imports = [
-    ./env.nix
-    ./ghostty.nix
+    # Editors
     ./nvim.nix
     ./vscodium.nix
+
+    # Shell, environment, terminal and cli apps
     ./zsh.nix
+    ./env.nix
+    ./ghostty.nix
     ./git.nix
     ./ollama.nix
     ./gnupg.nix
+
+    # Desktops, WM's...
+    # If you want to change this, change it in /etc/nixos/configuration.nix too.
     ./gnome.nix
     # ./plasma.nix
     # ./hyprland.nix
