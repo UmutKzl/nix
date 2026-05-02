cdea3a8745fcdb7a4121015e49a95c25a7151a7e
UmutKzl <umutkzl@protonmail.com>
2026-05-02 19:15:31 +0300

    feat: add some keybindings
    Super + 1,2,3,4,5,6,7,8,9 moves to workspaces
    Super + Shift + 1,2,3,4,5,6,7,8,9 moves windows to worksaces

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -41,6 +41,7 @@ in {
         variant = "frappe";
       };
     };
+
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.catppuccin-papirus-folders.override {
@@ -64,26 +65,63 @@ in {
       close = ["<Super>q"];
       toggle-maximized = ["<Super>k"];
       minimize = ["<Super>j"];
+
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      switch-to-workspace-5 = ["<Super>5"];
+      switch-to-workspace-6 = ["<Super>6"];
+      switch-to-workspace-7 = ["<Super>7"];
+      switch-to-workspace-8 = ["<Super>8"];
+      switch-to-workspace-9 = ["<Super>9"];
+
+      move-to-workspace-1 = ["<Super><Shift>1"];
+      move-to-workspace-2 = ["<Super><Shift>2"];
+      move-to-workspace-3 = ["<Super><Shift>3"];
+      move-to-workspace-4 = ["<Super><Shift>4"];
+      move-to-workspace-5 = ["<Super><Shift>5"];
+      move-to-workspace-6 = ["<Super><Shift>6"];
+      move-to-workspace-7 = ["<Super><Shift>7"];
+      move-to-workspace-8 = ["<Super><Shift>8"];
+      move-to-workspace-9 = ["<Super><Shift>9"];
     };
+
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+    };
+
     "org/gnome/mutter/keybindings" = {
       toggle-tiled-left = ["<Super>h"];
       toggle-tiled-right = ["<Super>l"];
     };
+
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       monospace-font-name = "JetBrainsMono Nerd Font 11";
       document-font-name = "JetBrainsMono Nerd Font 11";
       font-name = "JetBrainsMono Nerd Font 11";
     };
+
     "org/gnome/desktop/background" = {
       picture-uri = "file://${myWallpaper}";
       picture-uri-dark = "file://${myWallpaper}";
       picture-options = "zoom";
     };
+
     "org/gnome/desktop/peripherals/touchpad" = {
       natural-scroll = false;
       tap-to-click = false;
     };
+
     "org/gnome/mutter" = {
       experimental-features = [
         "scale-monitor-framebuffer"
@@ -91,14 +129,24 @@ in {
         "autoclose-xwayland"
       ];
     };
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"];
+
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 9;
     };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+      ];
+    };
+
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       name = "Launch Ghostty";
       command = "ghostty";
       binding = "<Super>Return";
     };
+
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
       name = "Launch Ulauncher";
       command = "ulauncher-toggle";
